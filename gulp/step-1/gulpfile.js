c0056ec609740a60109262e0c27899ae4ff749f7
caomin1204418950 <1204418950@qq.com>
2017-12-06 20:11:33 +0800

    '前端开发流'

---
+++ gulp/step-1/gulpfile.js
@@ -0,0 +1,4 @@
+var gulp = require('gulp');
+gulp.task('default',function(){
+    gulp.src('./src/main.js').pipe(gulp.dest('./dist'))
+})
---
+++ gulp/step-1/src/main.js
@@ -0,0 +1,6 @@
+// 入口文件
+
+
+const a=1;
+const b=2;
+console.log(a+b);
